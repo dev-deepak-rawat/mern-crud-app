109452acc440cbacedf0618afd766a34f5e5d0e3
Deepak Rawat <dev.deepak.rawat@gmail.com>
2021-07-21 21:27:58 +0530

    UPDATE: code refactored

--- server/middlewares/error.js
+++
@@ -1,5 +0,0 @@
-
-module.exports = (err, req, res, next) => {
-    console.log(err.message, err);
-    res.status(500).send('Something Failed.');
-};
--- server/startup/routes.js
+++ server/startup/routes.js
@@ -1,16 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 const users = require('../userRoute');
-const error = require('../middlewares/error');
 
 module.exports = function (app) {
     app.use(cors());
-    const corsOptions = {
-        exposedHeaders: 'Authorization',
-    };
 
-    app.use(cors(corsOptions));
+    app.use(cors());
     app.use(express.json());
     app.use('/users', users);
-    app.use(error);
 };
